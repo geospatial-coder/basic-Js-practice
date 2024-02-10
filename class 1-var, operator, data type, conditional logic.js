5727694690f0673aabff4d2804202c0ed3fb63e1
geospatial-coder <157166940+geospatial-coder@users.noreply.github.com>
2024-02-10 23:47:24 +0600

    uploaded first file

---
+++ class 1-var, operator, data type, conditional logic.js
@@ -0,0 +1,44 @@
+var a = "Ramiz"
+var b = 10 
+var booldata = true 
+var c = 9
+var d = b + c 
+// console.log(d)
+
+var e = 100 
+var f = " th positon: "
+// console.log(e.toString() + f)
+var g = "100"
+// console.log(Number(g))
+var oddNum = 5 
+var evenNum = "10" 
+console.log(oddNum+evenNum)
+console.log(oddNum + Number(evenNum))
+var firstName = "Ramiz"
+var lastName = "Moktader"
+var fullname = firstName +" " +  lastName
+console.log(fullname)
+
+
+// assignment operator "="
+//comparison operator 
+
+var a = "98"
+var b = 98
+var c = a===b 
+// console.log(c)
+
+var fname1 = "alex"
+var fname2 = "jhon"
+
+var t1 = true 
+var f1  = true 
+// console.log(t1 === f1)
+
+// Not equal 
+var var1 = 10990
+var var2 = 1008
+
+// console.log(var1 <=  var2 )
+var value = "Hello";
+var boolValue = Boolean("the value is ",value);  
